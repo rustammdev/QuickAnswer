0c721ff9c9a76cefc68d8981e5fbde87a937f7b6
Rustam Nuriddinov <rustamm.nuriddinov@gmai.com>
2024-07-15 20:18:34 +0500

    create event

--- controller/event.controller.js
+++ controller/event.controller.js
@@ -4,7 +4,7 @@ import eventServices from "../services/event.services.js";
 class EventController {
     async createEvent(req,res){
         const event = await  eventServices.createEvent(req.cookies.accessToken, req.body)
-        res.status(200).json({...event, ok : true, message : 'Successfully created'});
+        res.status(200).json({ ok : true, message : 'Successfully created'});
     }
 }
 
